b5563dbeb1869aad878197c14844b1984c327cc0
dream <dream@DESKTOP-91VBTBB>
2022-10-03 12:08:30 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeClasses
+{
+    internal class Circle
+    {
+        const double PI = 3.1415926535897931;
+        public double radius = 0;
+        public double diameter = 0;
+        string area = radius * radius * PI;
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+    
+namespace VolumeClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            class circle1 = new class
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
